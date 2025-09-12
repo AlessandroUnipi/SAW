776e4c4c7e71c15bf98df7377752d8f78aaaffa3
AlessandroUnipi <a.boghiu@studenti.unipi.it>
2025-09-12 16:38:34 +0200

    Progetto finito

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,7 +15,7 @@ const HomePage = () => {
       <div className="navigation-buttons">
         {/* freccia sinistra */}
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="http://www.w3.org/2000/svg" 
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
